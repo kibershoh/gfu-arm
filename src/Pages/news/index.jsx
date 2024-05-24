2d104e1a86d13a2326cd0e33dcec2954910cd12b
Oybek <saminovo150947@gmail.com>
2024-05-25 03:31:05 +0500

    add download btn

--- src/Pages/news/index.jsx
+++ src/Pages/news/index.jsx
@@ -67,6 +67,10 @@ const News = () => {
 
     fetchBooks();
   }, []);
+   const time = (date) => {
+    const day = `${formatDate(date.toDate().getDate())}.${formatDate(date.toDate().getMonth() + 1)}.${formatDate(date.toDate().getFullYear())}, ${formatDate(date.toDate().getHours())} : ${formatDate(date.toDate().getMinutes())}`
+    return day
+}
   return (
     <div className='mt-20'>
       {
@@ -84,7 +88,7 @@ const News = () => {
                   {/* <p className='pt-3'>{item.info}
 
                   </p> */}
-                  
+                  <span>{time(item.created)}</span>
                   <Modal info={item.info}/>
                 </div>
               ))
--- src/UI/book.jsx
+++ src/UI/book.jsx
@@ -23,17 +23,17 @@ const BookCard = ({ book }) => {
 
 
 
-        <div className="shadow rounded p-2 pt-4">
+        <div className="shadow rounded p-2 pt-4 pb-10">
           <div className='flex max-lg:flex-cols relative'>
             <img src={book.image} className='w-60 h-48 max-md:w-32 max-md:h-32 rounded-lg' />
-            <div className='p-5 pb-2 max-md:pl-2 max-md:pr-0 pt-0 w-full'>
+            <div className='p-5 pb-2 max-md:pl-2 max-md:pr-0 pt-0 w-full relative'>
               <h1 className='text-lg text-start  font-bold max-md:text-xs'>{book.title}</h1>
               <p className='text-slate-400 text-start w-full text-sm '>{book.author}</p>
               <p className='w-full pt-1 mt-3 overflow-y-auto  h-24 max-md:h-14 max-md:text-sm leading-4 font-normal'>
                 {book.caption}
               </p>
-             <div className='w-full text-right px-2 pt-3'>
-               <button className="inline-flex gap-5 items-center px-3 py-2 max-md:py-1 text-md max-md:text-sm font-medium text-center text-gray-800 border border-blue-600 rounded-md hover:bg-[#CAF4FF] focus:ring-4 focus:outline-none focus:ring-blue-300">
+             <div className='w-full text-right px-2 pt-8 absolute right-0 -bottom-8'>
+               <button className=" inline-flex gap-5 items-center px-3 py-2 max-md:py-1 text-md max-md:text-sm font-medium text-center text-gray-800 border border-blue-600 rounded-md hover:bg-[#CAF4FF] focus:ring-4 focus:outline-none focus:ring-blue-300">
                 Download <FaDownload />
 
               </button>
