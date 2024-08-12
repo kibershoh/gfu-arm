f8b0265ea067c3f8a5afd0f8c3e4b5e6239b8a7f
Oybek <saminovo150947@gmail.com>
2024-08-12 18:24:08 +0500

    optimise and finish project

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,48 @@ import NavbarAdmin from './Admin/Components/NavbarAdmin'
 import { useNavigate } from 'react-router-dom'
 
 const App = () => {
- 
 
-     const [data, setData] = useState([])
-    const navigate = useNavigate()
-    useEffect(() => {
-      const requestOptions = {
-        method: "GET",
-        redirect: "follow"
-      };
 
-      const fetchBooks = async () => {
-        try {
-          const response = await fetch("https://librarygfu.pythonanywhere.com/en-us/library/employee/", requestOptions);
-          const textResult = await response.json();
-          // setResult(textResult);
-          console.log(textResult);
-          setData(textResult.results);
-        } catch (err) {
-          setError(err);
-          console.error(err);
-        }
-      };
+  const [data, setData] = useState([])
+  const navigate = useNavigate()
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
 
-      fetchBooks();
-    },[]);
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch("https://librarygfu.pythonanywhere.com/en-us/library/employee/", requestOptions);
+        const textResult = await response.json();
+        // setResult(textResult);
+        console.log(textResult);
+        setData(textResult.results);
+      } catch (err) {
+        setError(err);
+        console.error(err);
+      }
+    };
+
+    fetchBooks();
+  }, []);
   return (
     <>
 
       {
-      location.pathname.startsWith("/admin")  ? <NavbarAdmin/> 
-      :
-       <>
-{
-  !location.pathname.startsWith("/newsdetails") ?
-      <Navbar/>
-      :''
-}
-      </>
-     }
+        location.pathname.startsWith("/admin") ? <NavbarAdmin />
+          :
+          <>
+            {
+              !location.pathname.startsWith("/newsdetails") ?
+                <Navbar />
+                : ''
+            }
+          </>
+      }
       <AllRouters />
       <Footer />
-        
+
     </>
   )
 }
--- src/Constants/CarouselImage/index.jsx
+++
@@ -1,6 +0,0 @@
-import one from '../../assets/education/1.jpg';
-import two from '../../assets/education/2.jpg';
-import three from '../../assets/education/4.jpg';
-export const IMAGES = [
- one,two,three,one,two
-]
