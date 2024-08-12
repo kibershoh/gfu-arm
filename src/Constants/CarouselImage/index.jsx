cdb56274e5e0f43c4b910039918770fe141cb78d
Oybek <saminovo150947@gmail.com>
2024-08-12 21:05:05 +0500

    don't error

---
+++ src/Constants/CarouselImage/index.jsx
@@ -0,0 +1,6 @@
+import one from '../../assets/education/1.jpg';
+import two from '../../assets/education/2.jpg';
+import three from '../../assets/education/4.jpg';
+export const IMAGES = [
+ one,two,three,one,two
+]
